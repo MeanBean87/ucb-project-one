edb79d7ab0b68b8ea47d0900b0c015552f6ff32e
Michael Mattingly <mikeamatt1@hotmail.com>
2023-07-14 16:36:11 -0400

    Performed code refactor, removed TODOs, added height conversion functions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 //TODO: Refine parameters to suit our needs.
 const fetchMealDbObj = async (query) => {
   const url =
@@ -45,112 +44,41 @@ const fetchCalorieNinjas = async (query) => {
   }
 };
 
-//TODO: create object to store activity level and multiplier.
 const activityLevel = {
   sedentary: 1.2, //(little to no exercise + work a desk job) = 1.2
   LightlyActive: 1.375, // (light exercise 1-3 days / week)
-  ModeratelyActive:  1.55, //(moderate exercise 3-5 days / week
+  ModeratelyActive: 1.55, //(moderate exercise 3-5 days / week
   VeryActive: 1.725, //(heavy exercise 5-7 days / week)
   ExtremelyActive: 1.9, //(very heavy exercise, hard labor job, training 2x / day)
 };
 
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//====================================================================================================
-//TODO: Convert feet and inches to base inches ie 6' 1' inches = 73.
 const convertImpHeightToBaseInches = (feet, inches) => {
   return feet * 12 + inches;
-}
+};
 
-const convertImpHeightToMetric = (inches) => {
+const convertInchesToCentimeters = (inches) => {
   return inches * 2.54;
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-//====================================================================================================
-// TODO: Function to calculate BMR for male.
-const calculateBMRMale = (weight, height, age, sex) => {
-  let 
-
-
-
- };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//====================================================================================================
-//TODO: Create function to convert imperial inches to metric centimeters.
-function convertInchesToCentimeters(inches) {
-  return inches * 2.54;
-}
-
-var heightInCentimeters = convertInchesToCentimeters(inches);
-
-
-//====================================================================================================
-//TODO: Create function to convert imperial pounds to metric kilograms.
-function convertPoundsToKilograms(pounds) {
+const convertPoundsToKilograms = (pounds) => {
   return pounds * 0.45359237;
-}
-var weightInKilograms = convertPoundsToKilograms(pounds);
+};
 
-
-//====================================================================================================
-//TODO: Function to calculate BMR for female.
-//Women BMR = 655 + (9.6 X weight in kg) + (1.8 x height in cm) – (4.7 x age in yrs)
-function femaleBMR(weight, height, age) {
+const calculateFemaleBMR = (weight, height, age) => {
   return 655 + 9.6 * weight + 1.8 * height - 4.7 * age;
-}
+};
 
-var femaleBMRResult = femaleBMR(weightInKilograms, heightInCentimeters, ageInYears);
-console.log(femaleBMRResult);
+const calculateMaleBMR = (weight, height, age) => {
+  return 66 + 13.7 * weight + 5 * height - 6.8 * age;
+};
+
+const calculateBMR = (weight, height, age, gender) => {
+  let weight = convertPoundsToKilograms(weight);
+  let height = convertInchesToCentimeters(convertImpHeightToBaseInches(height));
+
+  if (gender === "Female") {
+    return calculateFemaleBMR(weight, height, age);
+  } else {
+    return calculateMaleBMR(weight, height, age);
+  }
+};
