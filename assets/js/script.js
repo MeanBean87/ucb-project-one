fef20dab2cfb43e9e280bebbfc27ed265af10586
Joel <joellongaresjr@gmail.com>
2023-07-17 16:45:15 -0700

    created file for workoutplan + code to make it work + api key

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ import { mainContainer } from "./constants.js";
 
 const bodyContainer = document.querySelector(".body-container");
 const mealPlanGenerator = document.getElementById("meal-plan-generator");
+// const workoutPlanGenerator = document.getElementById("workout-plan-generator");
 const workoutPlanGenerator = document.getElementById("workout-plan-generator");
 const fetchExerciseObj = async (queryString) => {
   const apiKey = `KUNEX9M6Kwogj/J4y7Ru+A==FZ9J1FNl2AdRV6rw`;
@@ -12,25 +13,23 @@ const fetchExerciseObj = async (queryString) => {
 
   try {
     const response = await fetch(`${url}`, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'X-Api-Key': `${apiKey}`,  
-        'Content-Type': 'application/json'
-      }
+        "X-Api-Key": `${apiKey}`,
+        "Content-Type": "application/json",
+      },
     });
 
     if (!response.ok) {
-      throw new Error('Error: ' + response.status);
+      throw new Error("Error: " + response.status);
     }
 
     const result = await response.json();
     console.log(result);
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
   }
 };
-
-
 const createWorkout = () => {
   const workoutPlan = document.createElement("form");
   workoutPlan.setAttribute("id", "workout-plan");
@@ -41,14 +40,56 @@ const createWorkout = () => {
   );
   mainContainer.appendChild(workoutPlan);
 
-  const workoutPlanTitle = document.createElement("h2");
-  workoutPlanTitle.setAttribute("id", "workout-plan-title");
-  workoutPlanTitle.setAttribute("class", "workout-plan-title text-white");
-  workoutPlanTitle.textContent = "Type of Workout";
-  workoutPlan.appendChild(workoutPlanTitle);
+//   const workoutPlanTitle = document.createElement("h2");
+//   workoutPlanTitle.setAttribute("id", "workout-plan-title");
+//   workoutPlanTitle.setAttribute("class", "workout-plan-title text-white");
+//   workoutPlanTitle.textContent = "Type of Workout";
+//   workoutPlan.appendChild(workoutPlanTitle);
 
-};
+//   const workoutPlanDescription = document.createElement("p");
+//   workoutPlanDescription.setAttribute("id", "work-out-description");
+//   workoutPlanDescription.setAttribute(
+//     "class",
+//     "work-out-description text-white font-extrabold pb-5"
+//   );
+//   workoutPlanDescription.textContent =
+//     "Please select a workout type that you like!";
+//   workoutPlan.appendChild(workoutPlanDescription);
 
+//   //Form Workout Input
+//   const workoutPlanTypeInput = document.createElement("select");
+//   workoutPlanTypeInput.setAttribute("id", "workout-plan-type");
+//   workoutPlanTypeInput.setAttribute("class", "workout-plan-type");
+//   workoutPlan.appendChild(workoutPlanTypeInput);
+
+//   // WorkoutPlan Options
+//   const options = ["Strength", "Cardio", "Powerlifting"];
+//   for (const option of options) {
+//     const typeOption = document.createElement("option");
+//     typeOption.setAttribute("value", option);
+//     typeOption.textContent = option;
+//     workoutPlanTypeInput.appendChild(typeOption);
+//   }
+
+//   //Form Submit Button
+//   const workoutPlanSubmit = document.createElement("button");
+//   workoutPlanSubmit.setAttribute("type", "submit");
+//   workoutPlanSubmit.setAttribute("id", "workout-plan-submit");
+//   workoutPlanSubmit.setAttribute(
+//     "class",
+//     "workout-plan-submit text-white font-extrabold pb-5"
+//   );
+//   workoutPlanSubmit.textContent = "Submit";
+//   workoutPlan.appendChild(workoutPlanSubmit);
+// };
+
+
+createWorkout();
+// eventlistener submit to generate random workout form our workoutPlan Options
+workoutPlanSubmit.addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevent the form from submitting and refreshing the page
+  fetchExerciseObj(event.target.value);
+});
 workoutPlanGenerator.addEventListener("click", function (event) {
   event.preventDefault();
   clearMainContainer();
@@ -97,19 +138,11 @@ const homeLink = document.getElementById('home-link');
 
 homeLink.addEventListener('click', function (event) {
   event.preventDefault();
-<<<<<<< HEAD
-  clearMainContainer();
-  setTimeout(createTDEEQuestionnaire,1000);
-});
-
-
-=======
   setTimeout(() => {
     clearMainContainer(); createTDEEQuestionnaire();
   }, 2000); 
 });
 
->>>>>>> 2029e3c53b6bf5b53de4bd9803c72e624d14ea31
 mealPlanGenerator.addEventListener("click", function (event) {
   event.preventDefault();
   bodyContainer.i
@@ -118,4 +151,4 @@ mealPlanGenerator.addEventListener("click", function (event) {
 });
 //================================================================================================
 
-export { startFunction };
+export { startFunction, fetchExerciseObj };
